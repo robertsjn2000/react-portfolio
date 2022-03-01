5dee8158a3665fb35e0c3421ee9033b96c954a0d
robertsjn2000 <robertsjn2000@gmail.com>
2022-03-01 17:21:00 -0500

    final fix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,21 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 
 function Navigation(){
     return(
         <div>
             <ul className="nav navbar-nav navbar-right">
                 <li className="nav-item">
-                    <a href="/" className="nav-link">About Me</a>
+                    <Link href="/" className="nav-link">About Me</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="/portfolio" className="nav-link">Portfolio</a>
+                    <Link href="/portfolio" className="nav-link">Portfolio</Link>
                 </li>
                 <li className="nav-item">
                     <a href="https://docs.google.com/document/d/1cdxtch8rGiM-hXh1ZJYld5mzGSWA4GpBjMWkod1iL1g/edit?usp=sharing" className="nav-link">Resume</a>
                 </li>
                 <li className="nav-item">
-                    <a href="/contactMe" className="nav-link">Contact Me</a>
+                    <Link href="/contactMe" className="nav-link">Contact Me</Link>
                 </li>
             </ul>
         </div>
