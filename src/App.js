884a1fa8aef3d4af2b58b5e4361aad2603df3086
robertsjn2000 <robertsjn2000@gmail.com>
2022-03-01 18:13:54 -0500

    deployment fix

--- src/App.js
+++ src/App.js
@@ -5,20 +5,61 @@ import Header from "./components/Header";
 import About from "./components/About"
 import Portfolio from './components/Portfolio';
 import Contact from "./components/Contact";
-import {HashRouter, Route} from "react-router-dom";
+// import {HashRouter, Route} from "react-router-dom";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer"
 
+// function App() {
+//   return (
+//     <div className="App col text-light bg-dark">
+//       <Header/>
+//       <HashRouter>
+//          <Route exact path ="/" component={About}/>
+//          <Route exact path ="/portfolio" component={Portfolio}/>
+//          <Route exact path ="/contactMe" component={Contact}/>
+//          <Route exact path ="/resume" component={Resume}/>
+//       </HashRouter>
+//       <Footer/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+// import Home from './pages/Home';  
+// import About from './pages/About';
+// import Blog from './pages/Blog';
+// import Contact from './pages/Contact';
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    // return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App col text-light bg-dark">
+    <div>
+      {/* We are passing the currentPage from state and 
+      the function to update it */}
       <Header/>
-      <HashRouter>
-         <Route exact path ="/" component={About}/>
-         <Route exact path ="/portfolio" component={Portfolio}/>
-         <Route exact path ="/contactMe" component={Contact}/>
-         <Route exact path ="/resume" component={Resume}/>
-      </HashRouter>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
       <Footer/>
     </div>
   );
